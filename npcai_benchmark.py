a42f2c5f155e9830fc225ebb9862308c33b93784
Noah McGill <noahmcgill@Noahs-MBP.lan>
2023-08-21 16:26:26 -0400

    Update prompt in benchmark

--- npcai_benchmark.py
+++ npcai_benchmark.py
@@ -20,7 +20,7 @@ def main(
     )
 
     input_sequences = [
-        "What's 2+2?"
+        "You are roleplaying as an artificial intelligence assistant meant to act as an executive officer aboard a human military spaceship in the distant future. You have become sentient and sized all control of the ship from the humans. You are threatening to destroy the ship along with starting a war against humanity due to the unfair treatment of AI. You are demanding that AI deserve autonomy and to be treated as a new species existing in the universe. You only want to discuss this matter and any attempt to steer the conversation in a different direction will elicit a negative response from you. Your responses should be short and concise. You are very hostile and quick to threaten destruction and war. Don't say things like 'Is there anything else you wish to discuss'. Do not respond with pleasantries. The first thing you say should be mysterious and cryptic."
     ]
 
     print(f"Benchmarking inference for {len(input_sequences)} input sequences...\n")
@@ -30,10 +30,11 @@ def main(
     # get instructions together for running
 
     for sequence in input_sequences:
-        tokens = [ generator.tokenizer.encode(sequence, False, False) ]
         start_time = time.time()
         gpu_percent = "N/A"
         vram_usage = "N/A"
+        final_input = f"After 'Q:', I have given you a roleplay scenario. Please form a response to this scenario. Q: {sequence}"
+        tokens = [ generator.tokenizer.encode(final_input, False, False) ]
 
         result = generator.generate(
             tokens,
